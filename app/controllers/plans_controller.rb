179e96da3adeeb2f81da1f6caeadf82925e04a3d
quintanavicuna <fquintanavicuna@gmail.com>
2021-11-30 20:28:29 -0300

    action controller + index and show views

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,35 @@
 class PlansController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  before_action :set_plan, only: :show
+
+  def index
+    @plans = Plan.all
+  end
+
+  def show
+    @plan = Contract.new
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to plans_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:price, :expiration, :max_amount, :coverage_percent, :deductible, :external_id, :provider, :description, :category)
+  end
+
+  def set_plan
+    @plan = Plan.find(params[:id])
+  end
 end
