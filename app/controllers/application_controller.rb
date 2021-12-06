bb20503675d7c8258328150edeb7589bdbd93ac7
quintanavicuna <fquintanavicuna@gmail.com>
2021-12-06 17:42:51 -0300

    profile view + routes to profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
+    # For additional in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys:
       %i[email first_name last_name marital_status children dob budget])
