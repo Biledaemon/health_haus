04e670c2147a1434b192d4a1622d64fd257537f0
quintanavicuna <fquintanavicuna@gmail.com>
2021-12-07 12:48:13 -0300

    finishin demo day checklist

--- Gemfile
+++ Gemfile
@@ -29,7 +29,6 @@ gem 'redis', '~> 4.0'
 gem 'bootsnap', '>= 1.4.4', require: false
 
 gem 'devise'
-gem 'meta-tags'
 
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys:
       %i[email first_name last_name marital_status children dob budget])
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
