805c1d6ab9ec686b87752fc9b8baecd4bca02b41
Alfredo Montesinos <hugeyeti@protonmail.com>
2021-12-08 11:33:00 -0600

    Data parsed ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :plans, dependent: :destroy
+  has_many :contracts, dependent: :destroy
+  has_many :plans, through: :contracts
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
